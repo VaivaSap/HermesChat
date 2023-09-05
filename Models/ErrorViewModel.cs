381a199a8e6ec92cf963f4460b2a707b533f652c
AnnaJanu <anna.janusko@gmail.com>
2023-09-05 19:47:56 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HermesChat_TeamA.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+using HermesChat_TeamA.Areas.Identity.Data;
+using HermesChat_TeamA.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace HermesChat_TeamA
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+			var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+			builder.Services.AddDbContext<HermesChat_TeamA.Data.HermesChatDbContext>(options => options.UseSqlServer(connectionString));
+
+			builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = false).AddEntityFrameworkStores<HermesChat_TeamA.Data.HermesChatDbContext>();
+
+			// Add services to the container.
+			builder.Services.AddControllersWithViews();
+			builder.Services.AddRazorPages();
+
+			var app = builder.Build();
+
+			// Configure the HTTP request pipeline.
+			if (!app.Environment.IsDevelopment())
+			{
+				app.UseExceptionHandler("/Home/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
+
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
+
+			app.UseRouting();
+
+			app.UseAuthorization();
+
+			app.MapControllerRoute(
+				name: "default",
+				pattern: "{controller=Home}/{action=Index}/{id?}");
+			app.MapRazorPages();
+			app.Run();
+		}
+	}
+}
