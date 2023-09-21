bcc0c796254b0f497f892c0db249ceb71fff6e08
Vaiva <vaiva.sapetkaite@gmail.com>
2023-09-21 15:35:22 +0300

    Sending messages (to all and private ones) is working

--- Hubs/SyncHub.cs
+++ Hubs/SyncHub.cs
@@ -7,19 +7,17 @@ public class SyncHub : Hub
 {
     public async Task SendMessage(string user, string message)
     {
-      await  Clients.All.SendAsync("ReceiveMessage", user, message, DateTime.Now);
+      await  Clients.All.SendAsync("ReceiveMessage", Context.User.Identity.Name?? "anonymous", message, DateTime.Now);
     }
 
 
 
     public async Task SendToParticularUser(string user, string receiverConnectionId, string message)
     {
-        await Clients.Client(receiverConnectionId).SendAsync("ReceiveMessage", user, message);
+        await Clients.Client(receiverConnectionId).SendAsync("ReceiveMessage", Context.User.Identity.Name?? "anonymous", message);
     }
 
     public string GetConnectionId() => Context.ConnectionId;
   
 }
 
-
-//HermesChatDbContext ?
--- wwwroot/js/chat.js
+++ wwwroot/js/chat.js
@@ -1,5 +1,4 @@
-﻿
-"use strict";
+﻿"use strict";
 
 var connection = new signalR.HubConnectionBuilder().withUrl("/syncHub").build();
 
