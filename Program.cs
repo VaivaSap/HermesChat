bf6b0ff48d2bd3e05db5286af97783fca01bf17e
JurgitaPupalaigiene <40217338+JurgitaPupalaigiene@users.noreply.github.com>
2023-09-23 23:29:32 +0300

    HermisChat with working password reset

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
 using HermesChat_TeamA.Areas.Identity.Data;
 using HermesChat_TeamA.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace HermesChat_TeamA
 {
@@ -13,11 +17,41 @@ namespace HermesChat_TeamA
 
 			builder.Services.AddDbContext<HermesChat_TeamA.Data.HermesChatDbContext>(options => options.UseSqlServer(connectionString));
 
-			builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = false).AddEntityFrameworkStores<HermesChat_TeamA.Data.HermesChatDbContext>();
+			builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<HermesChat_TeamA.Data.HermesChatDbContext>();
+      
 
-			// Add services to the container.
-			builder.Services.AddControllersWithViews();
+            builder.Services.AddControllers();
+            builder.Services.Configure<SmtpSettings>(options => builder.Configuration.GetSection("EmailConfiguration")); 
+            
+          builder.Services.AddTransient<IEmailSender, EmailSender>();
+
+          
+
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Example API",
+                    Version = "v1",
+                    Description = "An example of an ASP.NET Core Web API",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Example Contact",
+                        Email = "example@example.com",
+                        Url = new Uri("https://example.com/contact"),
+                    },
+                });
+
+            });
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
 			builder.Services.AddRazorPages();
+           // builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+               // .AddCookie(option =>
+                //{
+                 //   option.LoginPath = "/Home/Index";
+                 //   option.ExpireTimeSpan = TimeSpan.FromMinutes(20);
+                //});
 
 			var app = builder.Build();
 
@@ -28,14 +62,23 @@ namespace HermesChat_TeamA
 				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 				app.UseHsts();
 			}
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
 
-			app.UseHttpsRedirection();
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                });
+            }
+
+            app.UseHttpsRedirection();
 			app.UseStaticFiles();
 
 			app.UseRouting();
-
+            app.UseAuthentication();
 			app.UseAuthorization();
-
+             
 			app.MapControllerRoute(
 				name: "default",
  				pattern: "{controller=Home}/{action=Index}/{id?}");
